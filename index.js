647cfa35800d33e3806987ee0aa900415a6aad8e
Siddhesh M <31335947+SId02@users.noreply.github.com>
2020-03-18 12:17:10 +0530

    i
    i

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+
+    res.render("home");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
